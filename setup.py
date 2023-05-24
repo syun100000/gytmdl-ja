140c5986e3b32031e011fec62c131cad05586000
Saito Shunsuke <50902764+syun100000@users.noreply.github.com>
2023-05-24 19:38:18 +0900

    update

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="gytmdlja",  # パッケージ名を指定します
-    version="0.1",  # バージョンを指定します
-    packages=find_packages(),  # 自動的にパッケージとサブパッケージを見つけます
-)
-
